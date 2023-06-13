46bf291a518779817cfb5cc3a27ad9581dd1ee25
Psawyer81 <136418145+Psawyer81@users.noreply.github.com>
2023-06-13 00:31:07 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "916363077501837000"; // your league ID
+export const leagueID = "784450885253193000"; // your league ID
 export const leagueName = "Ohio Fantasy"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
